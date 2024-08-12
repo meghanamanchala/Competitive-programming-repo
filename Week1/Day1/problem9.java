eaac483bffeb07a4f1708a2e40ef6b087f483b7a
Meghana <147796513+meghanamanchala@users.noreply.github.com>
2024-08-12 20:49:47 +0530

    Update problem9.java

--- Week1/Day1/problem9.java
+++ Week1/Day1/problem9.java
@@ -1,4 +1,4 @@
-// Problem 9: Find the Duplicate Number
+// Problem 9: Single Number
 
 // Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 // There is only one repeated number in nums, return this repeated number.
