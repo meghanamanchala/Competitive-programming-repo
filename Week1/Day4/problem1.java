38218ebd7bccd990d5a0b3202a6a90b05840f21c
Meghana <meghanamanchal@gmail.com>
2024-08-16 14:14:09 +0530

    day-4 week -1

---
+++ Week1/Day4/problem1.java
@@ -0,0 +1,34 @@
+
+//Problem - 1 : Longest Substring Without Repeating Characters
+
+//Question
+
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+//Solution
+
+class problem1 {
+    public int lengthOfLongestSubstring(String s) 
+    {
+        boolean[] charBool = new boolean[256];
+        int j = 0;
+        int maxLength = 0;
+        
+        for (int i=0;i<s.length();i++) 
+        {
+            char character = s.charAt(i);
+            while (charBool[character]) 
+            {
+                charBool[s.charAt(j)] = false;
+                j++;
+            }
+            charBool[character] = true; 
+            maxLength = Math.max(maxLength, i - j + 1);
+        }
+        return maxLength;
+    }
